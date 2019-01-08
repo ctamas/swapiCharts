5b8147b6c2c2d3f67c8eb99120999ec8a8784777
ctamas <ballin360x@gmail.com>
2019-01-08 10:58:12 +0000

    Updates

---
+++ precache-manifest.85f773396994a715172136950cef02a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07adf34fabf1c8fffbeb",
+    "url": "/swapiCharts/static/js/runtime~main.07adf34f.js"
+  },
+  {
+    "revision": "f474bb02185bf6d82907",
+    "url": "/swapiCharts/static/js/main.f474bb02.chunk.js"
+  },
+  {
+    "revision": "9511752e328af3937a5e",
+    "url": "/swapiCharts/static/js/1.9511752e.chunk.js"
+  },
+  {
+    "revision": "f474bb02185bf6d82907",
+    "url": "/swapiCharts/static/css/main.3918fdc5.chunk.css"
+  },
+  {
+    "revision": "9511752e328af3937a5e",
+    "url": "/swapiCharts/static/css/1.69eb9b9f.chunk.css"
+  },
+  {
+    "revision": "9e91618879225ea8498f8ab6fd26402b",
+    "url": "/swapiCharts/index.html"
+  }
+];
